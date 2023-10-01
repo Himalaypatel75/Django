e39e6872d9efe72fa235a5f64bb31e7a80546047
himalay patel <himalaypatel75@gmail.com>
2023-10-01 14:53:37 +0530

    basic crud operation completed

--- studybud/base/admin.py
+++ studybud/base/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Topic, Message
 
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name', 'description', 'updated', 'created')
     list_filter = ('updated', 'created')
     search_fields = ('name', 'description')
 
+
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('user', 'room', 'body', 'updated', 'created')
+    list_filter = ('updated', 'created')
+    search_fields = ('user__username', 'room__name', 'body')
+
 admin.site.register(Room, RoomAdmin)
+admin.site.register(Topic, TopicAdmin)
+admin.site.register(Message, MessageAdmin)
--- studybud/base/urls.py
+++ studybud/base/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home ,name="home"),
     path('rooms/<str:pk>/', views.room ,name="room"),
+    path('create-rooms/', views.create_room ,name="create-room"),
+    path('update-rooms/<str:pk>/', views.update_room ,name="update-room"),
+    path('delete-rooms/<str:pk>/', views.delete_room ,name="delete-room"),
 ]
